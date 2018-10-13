cc4ca856be93a97b1d0992c1aa55f45b1900c8c3
Ivan KOLESNIKOV <kiv.apple@gmail.com>
2018-10-13 14:13:10 +0400

    x86_bios drive detection draft

--- src/target/x86_bios/entry_point.c
+++ src/target/x86_bios/entry_point.c
@@ -42,19 +42,43 @@ void puts(const char *string) {
 	pm_functions->print_str(string);
 }
 
-void entry_point(PMFunctionTable *arg_pm_functions) {
-	pm_functions = arg_pm_functions;
+static void init_mm(void) {
 	const MemoryMap *mm = pm_functions->get_memory_map();
 	for (size_t i = 0; i < mm->entry_count; i++) {
 		if ((mm->entries[i].attrs & 1) && (mm->entries[i].type == 1) && (mm->entries[i].base >= 0x100000) && (mm->entries[i].length > 0)) {
 			mark_memory_as_free(mm->entries[i].base, mm->entries[i].length);
 		}
 	}
+}
+
+static void process_detected_drive(const DriveParameters *params) {
+	printf("valid=%u,id=0x%02x,drive_count=%u,edd_support=%u,spt=%u,head_count=%u,track_count=%u\r\n", 
+		   params->valid, params->id, params->drive_count, params->edd_support, params->spt, params->head_count, params->track_count);
+}
+
+static void detect_drives_range(uint8_t first_id) {
+	uint8_t drive_count = 1;
+	for (uint8_t id = first_id; id < first_id + drive_count; id++) {
+		DriveParameters params;
+		if (pm_functions->query_drive_parameters(id, &params)) {
+			drive_count = params.drive_count;
+			process_detected_drive(&params);
+		}
+	}
+}
+
+static void detect_drives(void) {
+	detect_drives_range(0x00);
+	detect_drives_range(0x80);
+}
+
+void entry_point(PMFunctionTable *arg_pm_functions) {
+	pm_functions = arg_pm_functions;
+	init_mm();
+	detect_drives();
 	
 	DriveParameters params;
 	pm_functions->query_drive_parameters(pm_functions->get_boot_drive_id(), &params);
-	printf("valid=%u,id=0x%02x,drive_count=%u,edd_support=%u,spt=%u,head_count=%u,track_count=%u\r\n", 
-		   params.valid, params.id, params.drive_count, params.edd_support, params.spt, params.head_count, params.track_count);
 	uint8_t *buffer = malloc(512);
 	if (buffer) {
 		bool result = pm_functions->read_sector(&params, 0, buffer);
